0fccd0bcd989123d88706d54cff20b070b8a5190
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-10-06 21:11:10 +0200

    wip

--- tree-sitter-pandoc-markdown/grammar.js
+++ tree-sitter-pandoc-markdown/grammar.js
@@ -1,13 +1,14 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
 // This grammar only concerns the block structure 
 // It is an extension of the tree-sitter-markdown grammar
-
 
 const MD = require('tree-sitter-markdown/tree-sitter-markdown/grammar');
 
 module.exports = grammar(MD, {
     name: 'pandoc_markdown',
     rules: {
-      ...MD.rules,
-    }
+      // ...MD.rules,
+  }
 })
 
