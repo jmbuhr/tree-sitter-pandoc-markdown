8ccab6093fedb4b695365f338c8d24ef36ae72bc
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-11-02 11:25:00 +0100

    wip on html code blocks prefixed by =

--- Makefile
+++ Makefile
@@ -2,4 +2,17 @@ all install uninstall clean:
 	$(MAKE) -C tree-sitter-pandoc-markdown $@
 	$(MAKE) -C tree-sitter-pandoc-markdown-inline $@
 
-.PHONY: all install uninstall clean
+test:
+	npm run build
+	$(MAKE) -C tree-sitter-pandoc-markdown $@
+	$(MAKE) -C tree-sitter-pandoc-markdown-inline $@
+
+qparse:
+	npm run build
+	cd tree-sitter-pandoc-markdown && tree-sitter parse 'test/highlight/quarto.qmd'
+
+qhighlight:
+	npm run build
+	tree-sitter highlight 'tree-sitter-pandoc-markdown/test/highlight/quarto.qmd'
+
+.PHONY: all install uninstall clean test
--- tree-sitter-pandoc-markdown/grammar.js
+++ tree-sitter-pandoc-markdown/grammar.js
@@ -5,10 +5,30 @@
 
 const MD = require('tree-sitter-markdown/tree-sitter-markdown/grammar');
 
+const common = require('tree-sitter-markdown/common/common');
+
 module.exports = grammar(MD, {
-    name: 'pandoc_markdown',
-    rules: {
-      // ...MD.rules,
+  name: 'pandoc_markdown',
+  rules: {
+    info_string: ($, md) =>
+      prec(2,
+        choice(
+          // md, // the original from the markdown grammar
+          choice(
+            seq(optional('='), $.language, repeat(choice($._line, $.backslash_escape, $.entity_reference, $.numeric_character_reference))),
+            seq(
+              repeat1(choice('{', '}')),
+              optional('='),
+              optional(choice(
+                seq($.language, repeat(choice($._line, $.backslash_escape, $.entity_reference, $.numeric_character_reference))),
+                seq($._whitespace, repeat(choice($._line, $.backslash_escape, $.entity_reference, $.numeric_character_reference))),
+              ))
+            )
+          )
+        )
+      ),
+    language: ($, _md) => prec.right(repeat1(choice($._word, common.punctuation_without($, ['{', '}', ',']), $.backslash_escape, $.entity_reference, $.numeric_character_reference))),
+    ...MD.rules
   }
 })
 
